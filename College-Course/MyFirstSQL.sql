704549473216a01b88a97ecaad1fa4a1d3f0d82e
Ali Sadeghi <ali909392@gmail.com>
2023-06-11 18:02:35 +0330

    Until 22/39

--- College-Course/MyFirstSQL.sql
+++ College-Course/MyFirstSQL.sql
@@ -1,6 +1,5 @@
 -- language: sql
 
-
 create database ali
 
 ------------------------------
@@ -49,6 +48,64 @@ create table person
     foreign key u_id reference randomTable (id),
     constraint admin check(age>=18 and id<20)
 )
+
+------------------------------
+
+CREATE TABLE Customer
+(
+    U_ID int NOT NULL PRIMARY KEY,
+    FirstName varchar(50) NOT NULL,
+    LastName varchar(50) NOT NULL,
+    Age int NOT NULL,
+    Address varchar(255) NOT NULL,
+    Country varchar(20) NOT NULL DEFAULT 'Iran',
+    City varchar(20) NOT NULL,
+    CHECK (Age>=18),
+    CONSTRAINT uc_customer UNIQUE (Age,LastName)
+)
+
+--  delete
+ALTER table Customer,
+constraint uc_customer drop,
+
+-- add
+ALTER table Customer,
+Add password int,
+
+-- modify
+ALTER table Customer
+ALTER column age date,
+
+------------------------------
+
+CREATE TABLE Customer
+(
+    U_ID int NOT NULL PRIMARY KEY identity,
+    FirstName varchar(50) NOT NULL,
+    LastName varchar(50) NOT NULL,
+    Age int NOT NULL,
+    Address varchar(255) NOT NULL,
+    Country varchar(20) NOT NULL DEFAULT 'Iran',
+    City varchar(20) NOT NULL,
+    CHECK (Age>=18),
+    CONSTRAINT uc_customer UNIQUE (Age,LastName)
+)
+
+Insert into Customer (first_name, last_name, age, Address, Country, city)
+values("ali", "sadeghi", 21, "iran-tehran", "iran", "tehran");
+
+Insert into Customer values("ali", "sadeghi", 21, "iran-tehran", "iran", "tehran")
+
+------------------------------
+
+select * from Customer
+
+update Customer
+set age = 17, first_name="saba"
+where u_id=5
+
+delete from Customer
+where u_id=5
 
 ------------------------------
 
